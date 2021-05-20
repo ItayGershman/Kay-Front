f9812836049ca27997fe4c9dc5070229b80ef5c1
AvielDagan <aviel.dagan@gmail.com>
2021-05-20 16:18:38 +0300

    Laser off script

--- Kay UI/LaserOff.py
+++ Kay UI/LaserOff.py
@@ -8,4 +8,5 @@ GPIO_LASER = 27
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(GPIO_LASER, GPIO.OUT)
-    GPIO.output(GPIO_LASER, 1)
+    GPIO.output(GPIO_LASER, 0)
+    GPIO.cleanup()
